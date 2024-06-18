a4f6d959b415bda2190e5413e0b4d8a252bbcab1
Haris Khan <haris90400@gmail.com>
2024-06-18 02:37:14 -0700

    flutter version configured

---
+++ client/lib/features/auth/viewmodel/auth_viewmodel.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthViewModel extends Notifier {
+  @override
+  build() {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
